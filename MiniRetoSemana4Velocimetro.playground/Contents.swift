26db0ff9a981f3b0272546d32784b0d0dec73098
Fernando Reyes Bejarano <fernandorb_8@hotmail.com>
2019-02-26 17:55:18 -0500

    Mini reto finalizado

--- MiniRetoSemana4Velocimetro.playground/Contents.swift
+++ MiniRetoSemana4Velocimetro.playground/Contents.swift
@@ -1,3 +1,38 @@
-import UIKit
+enum Velocidades : Int {
+    case Apagado = 0, VelocidadBaja = 20, VelocidadMedia = 50, VelocidadAlta = 120
+    init(velocidadInicial : Velocidades){
+        self = velocidadInicial
+    }
+}
 
-var str = "Hello, playground"
+class Auto {
+    var velocidad : Velocidades
+    init(){
+        self.velocidad = Velocidades(velocidadInicial : Velocidades.Apagado)
+    }
+    func cambioDeVelocidad() -> (actual : Int, velocidadEnCadena : String) {
+        var velocidadEnCadena = ""
+        switch velocidad{
+        case .Apagado:
+            self.velocidad = Velocidades.VelocidadBaja
+            velocidadEnCadena = "Velocidad Baja"
+        case .VelocidadBaja:
+            self.velocidad = Velocidades.VelocidadMedia
+            velocidadEnCadena = "Velocidad Media"
+        case .VelocidadMedia:
+            self.velocidad = Velocidades.VelocidadAlta
+            velocidadEnCadena = "Velocidad Alta"
+        case .VelocidadAlta:
+            self.velocidad = Velocidades.VelocidadMedia
+            velocidadEnCadena = "Velocidad Media"
+        }
+        return (self.velocidad.rawValue, velocidadEnCadena)
+    }
+}
+
+var auto = Auto()
+
+for _ in 1...20 {
+    let salida = auto.cambioDeVelocidad()
+    print("\(salida.actual),\(salida.velocidadEnCadena)")
+}
